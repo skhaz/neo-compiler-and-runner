dca547b6b6dff297a884cc3a618616023c4878c3
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-07-26 13:39:18 -0300

    Work in progress

--- main.go
+++ main.go
@@ -16,24 +16,24 @@ const (
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	var (
-		h      = openai.NewClient(os.Getenv("OPENAI_API_KEY"))
-		update = telegram.Parse(request.Body)
+		o = openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+		u = telegram.Parse(request.Body)
 	)
 
-	if strings.HasPrefix(update.Message.Text, prefix) {
+	if strings.HasPrefix(u.Message.Text, prefix) {
 		request := &openai.Request{
 			Model: openai.ModelGpt35Turbo,
 			Messages: []*openai.Message{
 				{Role: openai.RoleSystem, Content: "Keep strictly to say only the output, without any comment, what is the result of the following code?"},
-				{Role: openai.RoleUser, Content: strings.Trim(update.Message.Text, prefix)},
+				{Role: openai.RoleUser, Content: strings.Trim(u.Message.Text, prefix)},
 			}}
 
-		response, err := h.Do(request)
+		response, err := o.Do(request)
 		if err != nil || response.Error != nil {
 			return events.APIGatewayProxyResponse{StatusCode: 500}, err
 		}
 
-		telegram.Reply(os.Getenv("TELEGRAM_BOT_TOKEN"), update.Message.Chat.Id, response.Choices[0].Message.Content)
+		telegram.Reply(os.Getenv("TELEGRAM_BOT_TOKEN"), u.Message.Chat.Id, response.Choices[0].Message.Content)
 	}
 
 	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
