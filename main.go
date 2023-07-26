33bd3d0866cacc8dac93f936d6097c2696e4817e
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-07-26 09:44:52 -0300

    Work in progress

--- main.go
+++ main.go
@@ -1,43 +1,53 @@
 package main
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
-
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// Response is of type APIGatewayProxyResponse since we're leveraging the
-// AWS Lambda Proxy Request functionality (default behavior)
-//
-// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
-type Response events.APIGatewayProxyResponse
+type Chat struct {
+	Id int `json:"id"`
+}
 
-// Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(ctx context.Context) (Response, error) {
-	var buf bytes.Buffer
+type Message struct {
+	Text string `json:"text"`
+	Chat Chat   `json:"chat"`
+}
 
-	body, err := json.Marshal(map[string]interface{}{
-		"message": "Go Serverless v1.0! Your function executed successfully!",
-	})
-	if err != nil {
-		return Response{StatusCode: 404}, err
-	}
-	json.HTMLEscape(&buf, body)
+type Update struct {
+	UpdateId int     `json:"update_id"`
+	Message  Message `json:"message"`
+}
 
-	resp := Response{
-		StatusCode:      200,
-		IsBase64Encoded: false,
-		Body:            buf.String(),
-		Headers: map[string]string{
-			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "hello-handler",
-		},
-	}
+type Response struct {
+	Ok bool `json:"ok"`
+}
 
-	return resp, nil
+func Handler(update Update) (Response, error) {
+
+	return Response{Ok: true}, nil
+	/*
+		var buf bytes.Buffer
+
+		body, err := json.Marshal(map[string]interface{}{
+			"message": "Go Serverless v1.0! Your function executed successfully!",
+		})
+		if err != nil {
+			return Response{StatusCode: 404}, err
+		}
+		json.HTMLEscape(&buf, body)
+
+		resp := Response{
+			StatusCode:      200,
+			IsBase64Encoded: false,
+			Body:            buf.String(),
+			Headers: map[string]string{
+				"Content-Type":           "application/json",
+				"X-MyCompany-Func-Reply": "hello-handler",
+			},
+		}
+
+		return resp, nil
+	*/
 }
 
 func main() {
